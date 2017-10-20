8fece02c3aad63d31e1e612f5523d40af6fe2b03
Viachesalv Osadchiy <wavemeup1@gmail.com>
2017-10-20 20:48:40 +0300

    Handle VK error

--- common/services/vk.js
+++ common/services/vk.js
@@ -6,7 +6,9 @@ let config = require('../constants.json');
 module.exports.getUserProfile = (AccessToken) => {
   return new Promise((resolve, reject) => {
     request(config.vk.host+'users.get?fields=photo_max,city,verified&access_token='+AccessToken, (err, res, body) => {
-      resolve(JSON.parse(body).response[0]);
+      let data = JSON.parse(body);
+      if (data.error) reject(data.error.error_msg);
+      else resolve(data.response[0]);
     })
   })
 }
